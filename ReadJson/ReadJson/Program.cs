37e5971569cb5361de9813b1e6bc323221ca9669
MaiHoa <maithihoa09092003@gmail.com>
2023-12-28 00:18:12 +0700

    commit

---
+++ ReadJson/ReadJson/Program.cs
@@ -0,0 +1,26 @@
+﻿using ReadJson;
+using System.Collections.Generic;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        List<SinhVien> sinhViens = new List<SinhVien>();
+        ImportJs(sinhViens);
+    }
+
+    static void ImportJs(List<SinhVien> sinhViens)
+    {
+        var content = System.IO.File.ReadAllText(@"MOCK_DATA.json");
+        sinhViens = Newtonsoft.Json.JsonConvert.DeserializeObject<List<SinhVien>>(content);
+        Display(sinhViens);
+    }
+
+    static void Display(List<SinhVien> list)
+    {
+        foreach (var sinhVien in list)
+        {
+            sinhVien.Display();
+        }
+    }
+}
---
+++ ReadJson/ReadJson/SinhVien.cs
@@ -0,0 +1,14 @@
+﻿namespace ReadJson
+{
+    public class SinhVien
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine("id: {0}, first_name: {1}, last_name: {2}", id, first_name, last_name);
+        }
+    }
+}
